858630759c997df408f02a8b5dd89b010d568702
tgravey <38787197+Thibaut468@users.noreply.github.com>
2021-04-28 18:11:31 +0200

    feat: getProductHistory (hit me baby one more time)

--- contracts/transaction.js
+++ contracts/transaction.js
@@ -1,44 +1,23 @@
 const contract = require("@truffle/contract");
+const { json } = require("body-parser");
 
-const Transaction_artifact = require("./builds/transaction.json");
+const transaction_artifact = require("./builds/transaction.json");
+const actor_artifact = require("./builds/actor.json");
 
-var Transaction = contract(Transaction_artifact);
+var Transaction = contract(transaction_artifact);
+var Actor = contract(actor_artifact);
 
 // Following are functions which permit to have a JS abstract of the Smart Contract
 // and to interact with the ethereum blockchain
 // (i.e. create a new instance, deploy it, call its function, etc.)
 module.exports = {
-    getProductHistory: function (bcProductAdress, callback) {
-        var self = this;
-
+    getProductHistory: async function (bcProductAddress, web3) {
         // Bootstrap the Transaction abstraction for use
-        Transaction.setProvider(self.web3.currentProvider);
+        Transaction.setProvider(web3.currentProvider);
+        Actor.setProvider(web3.currentProvider);
 
-        self.web3.eth.getAccounts().then((accounts) => console.log(accounts));
-
-        // TODO : Work on the back track
-        // long, lat, Endroit, nom des acteurs
-        var result = [];
-
-        //Find the first transaction
-        Transaction.at(bcProductAdress).then(function (transaction) {
-            console.log(transaction);
-
-            result.push({
-                buyer: transaction.getBuyer().getName(),
-                buyerType: transaction.getBuyer().getActorType(),
-                buyerLat: transaction.getBuyer().getLatitude(),
-                buyerLong: transaction.getBuyer().getLongitude(),
-                seller: transaction.getSeller().getName(),
-                sellerType: transaction.getSeller().getType(),
-                sellerLat: transaction.getSeller().getLatitude(),
-                sellerLong: transaction.getSeller().getLongitude(),
-                transport: transaction.getTransport(),
-                date: transport.getDate(),
-            });
-
-            console.log(result);
-        });
+        // Backtrack on the product chain
+        var result = await getTransactionInformation(bcProductAddress);
 
         return JSON.stringify(result);
     },
@@ -46,4 +25,66 @@ module.exports = {
     // Accept an incoming transaction
 
     // Finish a pending transaction
+};
+
+getTransactionInformation = async (transactionAddress) => {
+    const result = [];
+
+    var jsonTransaction = {};
+
+    var transaction = await Transaction.at(transactionAddress);
+
+    var buyer = await Actor.at(await transaction.buyer());
+
+    jsonTransaction["id"] = await transaction.idTransaction();
+
+    jsonTransaction["buyer"] = {};
+    jsonTransaction["buyer"]["id"] = await buyer.id();
+    jsonTransaction["buyer"]["name"] = await buyer.name();
+    jsonTransaction["buyer"]["type"] = await buyer.actorType();
+    jsonTransaction["buyer"]["localisation"] = {};
+    jsonTransaction["buyer"]["localisation"]["latitude"] = await buyer.latitude();
+    jsonTransaction["buyer"]["localisation"]["longitude"] = await buyer.longitude();
+
+    var seller = await Actor.at(await transaction.seller());
+
+    jsonTransaction["seller"] = {};
+    jsonTransaction["seller"]["id"] = await seller.id();
+    jsonTransaction["seller"]["name"] = await seller.name();
+    jsonTransaction["seller"]["type"] = await seller.actorType();
+    jsonTransaction["seller"]["localisation"] = {};
+    jsonTransaction["seller"]["localisation"]["latitude"] = await seller.latitude();
+    jsonTransaction["seller"]["localisation"]["longitude"] = await seller.longitude();
+
+    jsonTransaction["productsInput"] = await transaction.getProductsInput();
+    jsonTransaction["productsOutput"] = await transaction.getProductsOutput();
+
+    jsonTransaction["transport"] = getTransportType(await transaction.transport());
+    let dateBN = await transaction.date();
+    jsonTransaction["date"] = dateBN.toNumber();
+    jsonTransaction["isFinished"] = await transaction.isFinished();
+    jsonTransaction["isAccepted"] = await transaction.isAccepted();
+
+    result.push(jsonTransaction);
+
+    console.log(jsonTransaction);
+
+    for (let i = 0; i < jsonTransaction.productsInput.length; i++) {
+        let address = jsonTransaction.productsInput[i].addressTransaction;
+        if (address !== "0x0000000000000000000000000000000000000000") {
+            result.concat(await getTransactionInformation(address));
+        }
+    }
+
+    return result;
+};
+
+getTransportType = (transportTypeId) => {
+    let enumTransportType = Transaction.TransportType;
+    for (const value of Object.entries(enumTransportType)) {
+        if (value[1] == transportTypeId) {
+            return value[0];
+        }
+    }
+    return null;
 };
