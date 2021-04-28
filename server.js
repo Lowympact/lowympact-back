d828b6a47b4da70ed529c60d6cb98d591d4ce6ba
lperrious <louis30@me.com>
2021-04-28 08:19:10 +0200

    feat: do not use CORS in dev mode

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const swaggerUi = require("swagger-ui-express");
 const connectDB = require("./connection/db");
 const connectBC = require("./connection/bc");
 const app = express();
+const simulation = require("./contracts/simulation");
 
 // Set PORT (default:8080)
 const PORT = process.env.port || 8080;
@@ -40,25 +41,26 @@ const allowedOrigins = [
     "http://api.lowympact.fr",
     "https://api.lowympact.fr",
 ];
+if (process.env.MODE !== "development") {
+    app.use(
+        cors({
+            origin: function (origin, callback) {
+                // allow requests with no origin
+                // (like mobile apps or curl requests)
+                if (!origin) return callback(null, true);
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    var msg =
+                        "The CORS policy for this site does not " +
+                        "allow access from the specified Origin.";
+                    return callback(new Error(msg), false);
+                }
 
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg =
-                    "The CORS policy for this site does not " +
-                    "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-
-            return callback(null, true);
-        },
-    })
-);
+                return callback(null, true);
+            },
+        })
+    );
+}
 
 // Body parser
 app.use(express.json()); //to support JSON-encoded bodies
@@ -103,64 +105,6 @@ app.get("/", function (req, res) {
 
 //Custom error handler
 app.use(errorHandler);
-
-// ?
-/*
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
-
-app.use(async (req, res, next) => {
-  if (req.headers["x-access-token"]) {
-    const accessToken = req.headers["x-access-token"];
-    try {
-      const { userId, exp } = await jwt.verify(
-        accessToken,
-        'O{egv"r&10Dné@fqpmdjvLdQhM:A00;FyszeG)_vHuQ8)Ig0éz,ORbÀ&&FMn bRHAw.D_kj;lkjq`7$T=k-<~ia-@erQP153'
-      );
-
-      // Check if token has expired
-      if (exp < Date.now().valueOf() / 1000) {
-        return res.status(401).json({
-          error: "JWT token has expired, please login to obtain a new one",
-        });
-      }
-      try {
-        res.locals.loggedInUser = await User.findById(userId);
-      } catch (err) {
-        res.status(err.code || 401);
-        res.json({ message: err.message || "Wrong userId" });
-      }
-      next();
-    } catch (err) {
-      res.status(err.code || 401);
-      res.json({ message: err.message || "Token expired" });
-    }
-  } else {
-    next();
-  }
-});
-
-app.use((req, res, next) => {
-  console.log(req, res);
-  throw new HttpError("Could not find this route.", 404);
-});
-
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
-});
-*/
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.MODE} on port ${PORT}`);
