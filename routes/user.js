c06128aec2cc3b39f8756ffd154d2f153680ef48
tgravey <38787197+Thibaut468@users.noreply.github.com>
2021-04-30 11:44:22 +0200

    doc: start swagger on user routes

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,9 @@ const express = require("express");
 const paginationFiltering = require("../middleware/paginationFiltering");
 const { protect, authorize } = require("../middleware/auth");
 const { checkApiKey } = require("../middleware/apiKey");
+const { checkJWT } = require("../middleware/jwtCheck");
 
-const {
-	getUsers,
-	getUser,
-	createUser,
-	updateUser,
-	deleteUser,
-} = require("../controllers/user");
+const { register, login, getUser, updateDetails, forgotPassword } = require("../controllers/user");
 
 const User = require("../models/user");
 
@@ -21,6 +16,204 @@ const router = express.Router();
 // router.use(authorize("admin"));
 // router.use(checkApiKey);
 
-router.route("/").get(paginationFiltering(User), getUsers).get(getUsers);
+// Get a specific user
+/**
+ * @swagger
+ * /api/v1/users/{userId}:
+ *  get:
+ *    summary: Gets a user by id
+ *    description: >
+ *     A detailed description of the user identified his id.
+ *     This route is protected and need the user to be auth.
+ *    produces:
+ *     - application/json
+ *    parameters:
+ *     - name: api-key
+ *      in: header
+ *      description: API-Key
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: true
+ *     - name: authorization
+ *      in: header
+ *      description: JWT Token for the user's session (can be stored in cookies)
+ *      schema:
+ *       type: string
+ *       format: uuid
+ *      required: false
+ *     - name: userId
+ *      in: path
+ *      description: User Identifier
+ *      type: string
+ *      required: true
+ *    responses:
+ *     200:
+ *      description: OK
+ *     404:
+ *      description: A user with the specified ID was not found.
+ */
+router.route("/:userId").get(checkJWT, getUser);
+
+// Modify user information and/or password
+/**
+ * @swagger
+ * /api/v1/users/{userId}:
+ *   put:
+ *     summary: Update/change user's informations
+ *     description: >
+ *        Update/change the username, email and password. For
+ *        the password, the current password must be given
+ *        This route is protected and need the user to be auth.
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *      - name: username
+ *        in: body
+ *        description: Username
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: false
+ * 		- name: email
+ *        in: body
+ *        description: Email
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: false
+ *      - name: currentPassword
+ *        in: body
+ *        description: Current password
+ *        type: string
+ *        required: false
+ *      - name: newPassword
+ *        in: body
+ *        description: New password
+ *        type: string
+ *        required: false
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.route("/:userId").put(checkJWT, updateDetails);
+
+// Register a new user
+/**
+ * @swagger
+ * /api/v1/users/:
+ *   post:
+ *     summary: Register a new user
+ *     description: Signup a new user in the application
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *      - name: api-key
+ *        in: header
+ *        description: API-Key
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: true
+ *      - name: username
+ *        in: body
+ *        description: User ID
+ *        type: string
+ *        required: true
+ * 		- name: email
+ *        in: body
+ *        description: User email
+ *        schema:
+ *          type: string
+ *          format: email
+ *        required: true
+ * 		- name: password
+ *        in: body
+ *        description: User password
+ *        schema:
+ *          type: string
+ *          format: password
+ *        required: true
+ *     responses:
+ *       201:
+ *         description: CREATED
+ * 		 400:
+ * 		   description: A user with the specified ID already exist
+ */
+router.route("/").post(register);
+
+// Login
+/**
+ * @swagger
+ * /api/v1/users/login:
+ *   post:
+ *     summary: Login a user
+ *     description: Signin a user in the application and get the JWT Token for cookies
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *      - name: api-key
+ *        in: header
+ *        description: API-Key
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: true
+ *      - name: username
+ *        in: body
+ *        description: User ID
+ *        type: string
+ *        required: true
+ * 		- name: password
+ *        in: body
+ *        description: User password
+ *        schema:
+ *          type: string
+ *          format: password
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ * 		 404:
+ * 		   description: A user with the specified ID was not found.
+ */
+router.route("/login").post(login);
+
+// Request password reset
+/**
+ * @swagger
+ * /api/v1/users/forgot-password:
+ *   get:
+ *     summary: Start password reset
+ *     description: Start a password reset process for a user
+ *     produces:
+ *      - application/json
+ *     parameters:
+ *      - name: api-key
+ *        in: header
+ *        description: API-Key
+ *        schema:
+ *          type: string
+ *          format: uuid
+ *        required: true
+ *      - name: username
+ *        in: body
+ *        description: User ID
+ *        type: string
+ *        required: true
+ * 		- name: password
+ *        in: body
+ *        description: User password
+ *        schema:
+ *          type: string
+ *          format: password
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ * 		 404:
+ * 		   description: A user with the specified ID was not found.
+ */
+router.route("/forgot-password").get(forgotPassword);
 
 module.exports = router;
