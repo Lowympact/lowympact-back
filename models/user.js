516f399d491eaa8352a06fea077668bfcedd754a
tgravey <38787197+Thibaut468@users.noreply.github.com>
2021-04-30 10:56:46 +0200

    feat: user model

--- models/user.js
+++ models/user.js
@@ -3,58 +3,64 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, "Please add a name"],
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email",
+    username: {
+        type: String,
+        required: [true, "Please add a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please add a valid email"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false, // not return the password
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    history: [
+        {
+            productInformations: [
+                {
+                    barcode: {
+                        type: String,
+                    },
+                    bcProductAddress: {
+                        type: String,
+                    },
+                },
+            ],
+        },
     ],
-  },
-  role: {
-    type: String,
-    enum: ["proprietaire", "utilisateur", "admin", "tenant"],
-    required: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-    minlength: 6,
-    select: false, // not return the password
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
 });
 
 //Encrypt password
 UserSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt(10);
-  //middleware has access to the fields
-  this.password = await bcrypt.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    //middleware has access to the fields
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 //Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
-  //we have access to the user id since this is a method
-  //options: expiresIn
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+    //we have access to the user id since this is a method
+    //options: expiresIn
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
 };
 
 //Match user entered password to hashed password in DB
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model("User", UserSchema);
