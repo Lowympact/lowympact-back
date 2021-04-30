abc976d13c06c874a28d9c32a74d90ec2255ef2c
tgravey <38787197+Thibaut468@users.noreply.github.com>
2021-04-30 10:34:25 +0200

    feat: add Allow credentials

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
     );
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
     next();
 });
