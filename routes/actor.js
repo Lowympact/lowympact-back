0cfb6de2b2b09436dad8fd5ef46f5bd1d208722c
lperrious <louis30@me.com>
2021-04-30 21:28:56 +0200

    doc: add Swagger for Actor's routes

--- routes/actor.js
+++ routes/actor.js
@@ -16,168 +16,195 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 // anything below will use the middleware
-//router.use(protect);
-//router.use(authorize("admin"));
 // router.use(checkApiKey);
 
 /**
- * //@swagger
- * /api/v1/actors:
- *  post:
- *      summary: Register a new actor
- *      description: Register a new actor on the application
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: object
- *                          properties:
- *                              email:
- *                                  type: string
- *                              name:
- *                                  type: string
- *                              password:
- *                                  type: string
- *      produces:
- *          - application/json
- *      parameters:
- *        - name: authorization
- *          in: header
- *          description: API-Key with Bearer prefix
- *          schema:
- *              type: string
- *              format: uuid
- *              required: true
- *       responses:
- *          200:
- *              description: OK
+ * @swagger
+ * /actors/:
+ *   post:
+ *     summary: Register a new actor
+ *     tags:
+ *       - actors
+ *     description: Signup a new company in the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: api-key
+ *         in: header
+ *         description: API-Key
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *           required: true
+ *       - name: name
+ *         in: body
+ *         description: Actor's name
+ *         schema:
+ *           type: string
+ *           required: true
+ *       - name: email
+ *         in: body
+ *         description: Actor's email
+ *         schema:
+ *           type: string
+ *           format: email
+ *           required: true
+ *       - name: password
+ *         in: body
+ *         description: Actor's password
+ *         schema:
+ *           type: string
+ *           format: password
+ *           required: true
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: An actor with the specified email already exist
  */
 router.route("/").post(registerActor);
 
 /**
- * //@swagger
- * /api/v1/actors:
+ * @swagger
+ * /actors/login:
  *   post:
  *     summary: Login an actor
- *     description: >
- *        Login a new actor on the application
+ *     tags:
+ *       - actors
+ *     description: Signin an actor in the application and get the JWT Token relatives to the session
  *     produces:
- *      - application/json
+ *       - application/json
  *     parameters:
- *      - name: authorization
- *        in: header
- *        description: API-Key with Bearer prefix
- *        schema:
- *          type: string
- *          format: uuid
- *        required: true
- *      - name: email
- *        in: body
- *        description: email of the actor
- *        type: string
- *        required: true
- *      - name: password
- *        in: body
- *        description: Password of the actor to connect and decrypt wallet
- *        type: string
- *        required: true
- *     responses:
- *       200:
- *         description: jwt
- */
-router.route("/login").post(loginActor);
-/**
- * //@swagger
- * /api/v1/actors:
- *   get:
- *     summary: Get an actor by id
- *     description: >
- *        Get actor's information
- *     produces:
- *      - application/json
- *     parameters:
- *      - name: authorization
- *        in: header
- *        description: API-Key with Bearer prefix
- *        schema:
- *          type: string
- *          format: uuid
- *        required: true
- *      - name: jwt
- *        in: header
- *        description: Authentification of the actor
- *        type: JWT
- *        required: true
- *      - name: actorId
- *        in: path
- *        description: id of the actor
- *        type: string
- *        required: true
+ *       - name: api-key
+ *         in: header
+ *         description: API-Key
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *           required: true
+ *       - name: email
+ *         in: body
+ *         description: actor's email
+ *         schema:
+ *           type: string
+ *           format: email
+ *           required: true
+ *       - name: password
+ *         in: body
+ *         description: actor's password
+ *         schema:
+ *           type: string
+ *           format: password
+ *           required: true
  *     responses:
  *       200:
  *         description: OK
+ *       404:
+ *         description: No actor with the specified email was found.
+ */
+router.route("/login").post(loginActor);
+
+/**
+ * @swagger
+ * /actors/{idActor}:
+ *   get:
+ *     summary: Gets an actor by id
+ *     tags:
+ *       - actors
+ *     description: >
+ *       A detailed description of the actor identified by his id.
+ *       This route is protected and needs the user to be auth.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: api-key
+ *         in: header
+ *         description: API-Key
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *           required: true
+ *       - name: authorization
+ *         in: header
+ *         description: JWT Token for the actor's session
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *           required: false
+ *       - name: idActor
+ *         in: path
+ *         description: Actor identifier
+ *         schema:
+ *           type: string
+ *           required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: No actor with the specified ID was found.
  */
 router.route("/:idActor").get(checkJWT, getActor);
 
 /**
- * //@swagger
- * /api/v1/actors:
+ * @swagger
+ * /actors/{idActor}:
  *   put:
- *     summary: Update the actor's model
+ *     summary: Update actor's informations
+ *     tags:
+ *       - actors
  *     description: >
- *        Update the actor's related informations
+ *        Update the name and/or the password.
+ *        This route is protected and needs the user to be auth.
  *     produces:
- *      - application/json
+ *       - application/json
  *     parameters:
- *      - name: authorization
- *        in: header
- *        description: API-Key with Bearer prefix
- *        schema:
- *          type: string
- *          format: uuid
- *        required: true
- *      - name: jwt
- *        in: header
- *        description: Authentification of the actor
- *        type: JWT
- *        required: true
- *      - name: actorId
- *        in: query
- *        description: id of the actor
- *        type: string
- *        required: true
+ *       - name: name
+ *         in: body
+ *         description: Actor's name
+ *         schema:
+ *           type: string
+ *           required: false
+ *       - name: email
+ *         in: body
+ *         description: Actor's email
+ *         schema:
+ *           type: string
+ *           format: email
+ *           required: false
+ *       - name: currentPassword
+ *         in: body
+ *         description: Current password
+ *         schema:
+ *           type: string
+ *           format: password
+ *           required: false
+ *       - name: newPassword
+ *         in: body
+ *         description: New password
+ *         schema:
+ *           type: string
+ *           required: false
  *     responses:
  *       200:
- *         description: OK
+ *        description: OK
  */
 router.route("/:idActor").put(checkJWT, updateActor);
 
 /**
- * //@swagger
- * /api/v1/actors/{idActor}:
+ * @swagger
+ * /actors/{idActor}:
  *   delete:
- *     summary: Delete an actor
+ *     summary: Delete an actor account
+ *     tags:
+ *       - actors
  *     description: >
- *        Delete an existing actor of the application
+ *        Delete an actor account from the database.
+ *        This route is protected and need the user to be auth.
  *     produces:
- *      - application/json
- *     parameters:
- *      - name: authorization
- *        in: header
- *        description: API-Key with Bearer prefix
- *        schema:
- *          type: string
- *          format: uuid
- *        required: true
- *      - name: jwt
- *        in: header
- *        description: Authentification of the actor
- *        type: JWT
- *        required: true
+ *       - application/json
  *     responses:
  *       200:
- *         description: OK
+ *        description: OK
  */
 router.route("/:idActor").delete(checkJWT, deleteActor);
 
