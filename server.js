1fe9a90a6e11cea53cb581cc84df76d0f316ff19
koechslin <killian.oechslin@insa-lyon.fr>
2021-05-03 08:52:22 +0200

    feat: add PUT in Access-Control-Allow-Methods

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
         "Origin, X-Requested-With, Content-Type, Accept,api-key, Authorization, x-access-token"
     );
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS, PUT");
     res.setHeader("Access-Control-Allow-Credentials", true);
 
     next();
